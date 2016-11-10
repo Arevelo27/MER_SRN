da04264f7ad2feca5b95e247364d72dacb41fd85
Andres Felipe Revelo <ing2013andresfe@gmail.com>
2016-11-10 01:04:38 -0500

    CAMBIOS EN TBL_NOTAS
    Cambios en las columnas de la tabla tbl_notas

--- srn_mer.sql
+++ srn_mer.sql
@@ -99,16 +99,14 @@ CREATE TABLE srn_tbl_nota
   (
     num_id_nota         NUMBER (10) NOT NULL ,
     num_cod_materiauser NUMBER (10) NOT NULL ,
-    num_parcial_I       NUMBER (2,2) ,
-    num_parcial_II      NUMBER (2,2) ,
-    num_parcial_III     NUMBER (2,2) ,
-    num_nota_adicional  NUMBER (2,2) ,
-    num_pryecto         NUMBER (2,2) ,
-    num_nota_final      NUMBER (2,2)
+    num_parcial_I       DECIMAL(2,2) ,
+    num_parcial_II      DECIMAL (2,2) ,
+    num_parcial_III     DECIMAL (2,2) ,
+    num_nota_adicional  DECIMAL (2,2) ,
+    num_pryecto         DECIMAL (2,2) ,
+    num_nota_final      DECIMAL (3,2)
   ) ;
 ALTER TABLE srn_tbl_nota ADD CONSTRAINT srn_tbl_nota_PK PRIMARY KEY ( num_id_nota ) ;
-
-
 
 ALTER TABLE srn_tbl_menu_permisos ADD CONSTRAINT FK_tbl_menu_per_tbl_rol FOREIGN KEY ( num_cod_rol ) REFERENCES srn_tbl_rol ( num_id_rol ) ;
 
